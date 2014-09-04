5976b023756a0b91dfe3d183740a03ebd425302d
Syed Fazle Rahman <fazlerahman786@gmail.com>
2014-09-05 02:17:43 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,3 @@
+angular.module("animationApp",["ngAnimate"]).controller("FruitsController", function($scope){
+	$scope.fruits = ["Apple","Apricot","Avocado","Banana","Breadfruit","Bilberry","Blackberry","Blackcurrant","Blueberry","Boysenberry","Cantaloupe","Currant","Cherry","Cherimoya","Cloudberry","Coconut","Cranberry","Cucumber","Damson","Date","Dragonfruit","Durian"];
+});
